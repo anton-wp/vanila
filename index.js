1990118868820e9db69250e1e3dcd8a8c7f64a5d
anton-wp <anton69917091@gmail.com>
2020-06-30 21:38:53 +0300

    tasksBlock

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import './style.scss'
 
 let addButton = document.getElementById('add');
-let inputTask = document.getElementById('new-task');
-let tasks = document.getElementById('tasks');
+let addBlockButton = document.getElementById('addBlock');
+// let inputTask = document.getElementById('new-task');
+// let tasks = document.getElementById('tasks');
+let container = document.getElementById('container');
+
+let currentCount = 1;
+
+function makeCounter() {
+  return function() {
+    return currentCount++;
+  };
+}
+
+let counter = makeCounter();
 
 let createTask = (task) => {
     let listItem = document.createElement('li');
@@ -18,23 +30,59 @@ let createTask = (task) => {
     input.type = "text";
 
     listItem.appendChild(label);
+    listItem.appendChild(input);
     listItem.appendChild(buttonDel);
     listItem.appendChild(editDel);
-    listItem.appendChild(input);
 
     return listItem;
 }
-
-let addTask = () => {
-    if (inputTask.value) {
-        let listItem = createTask(inputTask.value);
+function addTask() {
+    console.log(this.previousSibling )
+    let tasks = this.parentNode.parentNode.lastChild
+    let value = this.parentNode.firstChild.value 
+    if (value) {
+        let listItem = createTask(value);
         tasks.appendChild(listItem);
-        inputTask.value = '';
+        this.parentNode.firstChild.value = '';
         bindTaskEvents(listItem)
+        // console.log(this.parentNode.firstChild)
     }
 }
 
-addButton.onclick = addTask;
+let createBlockTask = () => {
+    let listItem = document.createElement('div');
+    listItem.className = 'row'
+    listItem.id = counter()
+    let divAddBlock = document.createElement('div');
+    divAddBlock.className = 'addBlock'
+    let input = document.createElement('input');
+    input.type = "text";
+    input.id = `new-task`;
+    let buttonAdd = document.createElement('button');
+    buttonAdd.id = "add"
+    buttonAdd.innerText = "Add"
+    buttonAdd.onclick = addTask;
+    let Ul = document.createElement('ul')
+    Ul.id = "tasks"
+
+    divAddBlock.appendChild(input);
+    divAddBlock.appendChild(buttonAdd);
+
+    listItem.appendChild(divAddBlock);
+    listItem.appendChild(Ul);
+    
+    return listItem;
+}
+
+
+let addBlockTask = () => {
+    let listItem = createBlockTask();
+    container.appendChild(listItem);
+    // bindTaskEvents(listItem)
+}
+
+// addButton.onclick = addTask;
+addBlockButton.onclick = addBlockTask;
 
 function deleteTask() {
     let listItem = this.parentNode;
@@ -46,12 +94,10 @@ function editTask() {
     let listItem = this.parentNode;
     let label = listItem.querySelector('label');
     let input = listItem.querySelector('input[type=text]');
-    // let input = listItem.querySelector('input[type=text]');
 
     let containsClass = listItem.classList.contains('editMode')
-    console.log(listItem)
-    console.log(label)
-    if(containsClass){
+
+    if (containsClass) {
         label.innerText = input.value;
     } else {
         input.value = label.innerText;
@@ -64,5 +110,4 @@ let bindTaskEvents = (listItem) => {
 
     editButton.onclick = editTask;
     delButton.onclick = deleteTask;
-    //first
 }
